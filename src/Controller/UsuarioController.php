0d8e934bdf04e8a1f53c2fc233d95b174816bdad
CAHM-UFPS <camilohdez1396@gmail.com>
2022-08-25 17:36:14 -0500

    Correccion de estilo

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -14,13 +14,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class UsuarioController extends AbstractController
 {
     #[Route('/create', name: 'create_user', methods: ['POST'])]
-    public function create(Request $request, UsuarioRepository $usuarioRepository) : JsonResponse
+    public function create(Request $request, UsuarioRepository $usuarioRepository): JsonResponse
     {
-        $user=new Usuario(); //Instancio el objeto usuario
-        $form=$this->createForm(UsuarioType::class, $user); //Creamos el formulario con la clase Type
+        $user = new Usuario(); //Instancio el objeto usuario
+        $form = $this->createForm(UsuarioType::class, $user); //Creamos el formulario con la clase Type
         $form->submit($request->toArray()); //Enviamos data convirtiendo los datos del request en arreglo
 
-        if($form->isSubmitted() && $form->isValid()){ //Validamos los datos y si hemos enviado data
+        if ($form->isSubmitted() && $form->isValid()) { //Validamos los datos y si hemos enviado data
             $usuarioRepository->add($user, true); //Añadimos a la base de datos
             return $this->json($user); //Retornamos el usuario creado como json
         }
@@ -29,16 +29,16 @@ class UsuarioController extends AbstractController
     }
 
     #[Route('/{id}', name: 'edit_user', methods: ['PUT'])]
-    public function update(Usuario $user=null, Request $request, UsuarioRepository $usuarioRepository) : JsonResponse
+    public function update(Usuario $user = null, Request $request, UsuarioRepository $usuarioRepository): JsonResponse
     {
-        if(!$user){
-            return $this->json(['message'=>'user not found'], 404);
+        if (!$user) {
+            return $this->json(['message' => 'user not found'], 404);
         }
 
-        $form=$this->createForm(UsuarioType::class, $user);
+        $form = $this->createForm(UsuarioType::class, $user);
         $form->submit($request->toArray());
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $usuarioRepository->add($user, true);
             return $this->json($user);
         }
